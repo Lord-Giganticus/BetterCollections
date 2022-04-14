86798aec2912072bedaf9b1922e8f6ff84a5cd36
Lord-Giganticus <70081529+Lord-Giganticus@users.noreply.github.com>
2022-04-14 07:41:30 -0600

    add util classes

---
+++ BetterCollections/DictUtil.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BetterCollections
+{
+    public static class DictUtil
+    {
+        public static Dictionary<TKeyN, TValueN> Change<TKey, TValue, TKeyN, TValueN>(this Dictionary<TKey, TValue> dict, BetterDictionary<TKey, TValue>.ChangeDelegate<TKeyN, TValueN> func)
+        {
+            var cast = (BetterDictionary<TKey, TValue>)dict;
+            return cast.Change(func);
+        }
+
+        public static long GetAllHashCode<TKey, TValue>(this Dictionary<TKey, TValue> dict)
+        {
+            long res= 0;
+            var hashes = dict.Change(x => new KeyValuePair<int, int>(x.Key.GetHashCode(), x.Value.GetHashCode()));
+            foreach (var hash in hashes)
+            {
+                res += hash.Key;
+                res += hash.Value;
+            }
+            return res;
+        }
+    }
+}
---
+++ BetterCollections/ListUtil.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BetterCollections
+{
+    public static class ListUtil
+    {
+        public static List<TNew> Change<TOld, TNew>(this List<TOld> list, BetterList<TOld>.ChangeDelegate<TNew> func)
+        {
+            var cast = (BetterList<TOld>)list;
+            return cast.Change(func);
+        }
+
+        public static long GetAllHashCode<T>(this List<T> list)
+        {
+            long res = 0;
+            var hashs = list.Change(x => x.GetHashCode());
+            hashs.ForEach(x => res += x);
+            return res;
+        }
+    }
+}
